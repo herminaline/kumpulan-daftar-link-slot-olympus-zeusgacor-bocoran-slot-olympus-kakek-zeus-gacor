85d6ca8e717ee4e1c169557df37ded9dea1eb980
herminaline <xenamarteno7@gmail.com>
2022-08-08 18:03:24 +0700

    first commit

---
+++ spec/kumpulan-daftar-link-slot-olympus-zeusgacor-bocoran-slot-olympus-kakek-zeus-gacor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KumpulanDaftarLinkSlotOlympusZeusgacorBocoranSlotOlympusKakekZeusGacorView from '../lib/kumpulan-daftar-link-slot-olympus-zeusgacor-bocoran-slot-olympus-kakek-zeus-gacor-view';
+
+describe('KumpulanDaftarLinkSlotOlympusZeusgacorBocoranSlotOlympusKakekZeusGacorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
